5357cd72f4b9b34c515f5668ecb44c2b14680eb1
Cahya Gumilang <cahyagumilang0@gmail.com>
2023-06-04 13:51:00 +0700

    Add error message

--- src/middlewares/verifiyToken.js
+++ src/middlewares/verifiyToken.js
@@ -1,12 +1,21 @@
 const jwt = require("jsonwebtoken");
+const ResponseClass = require("../utils/response.js");
 
 //verify token for login authorization function
 const verifyToken = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-  if (token == null) return res.sendStatus(401);
+  const responseError = new ResponseClass.ErrorResponse();
+  const token = req.headers["authorization"];
+  if (token == null) {
+    responseError.message = "You are not authorized";
+    responseError.code = 401;
+    return res.json(responseError);
+  }
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) return res.sendStatus(403);
+    if (err) {
+      responseError.message = err.message;
+      responseError.code = 403;
+      return res.json(responseError);
+    }
     req.userId = decoded.userId;
     req.email = decoded.email;
     next();
